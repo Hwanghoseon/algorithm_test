dd7999655949fed559578a7712e3587b374614db
Hwanghoseon <49743109+Hwanghoseon@users.noreply.github.com>
2020-02-03 19:53:17 +0900

    백준 순열 (다음 순열)

---
+++ 10972.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int n;
+vector<int> v;
+
+void printNext() {
+	if (next_permutation(v.begin(), v.end()))
+		for (int i = 0; i < n; i++)
+			cout << v[i] << " ";
+	else cout << "-1";
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	printNext();
+
+	return 0;
+}
