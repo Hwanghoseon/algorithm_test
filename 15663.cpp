91fbe3a9d94428b5e2f9dd7ba0dbad2648939d41
Hwanghoseon <49743109+Hwanghoseon@users.noreply.github.com>
2020-02-11 18:27:39 +0900

    백준 백트래킹 - 순열 (N과 M(9))

---
+++ 15663.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+const int MAX = 8;
+
+vector<int> arr;
+vector<int> list;
+bool visit[MAX];
+int n, m;
+
+void init() {
+	cin >> n >> m;
+
+	arr.resize(n);
+	list.resize(n);
+
+	for (int i = 0; i < n; i++)
+		cin >> list[i];
+
+	sort(list.begin(), list.end());
+}
+
+void func(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << " ";
+		cout << "\n";
+
+		return;
+	}
+
+	int before = -1;
+	for (int i = 0; i < n; i++) {
+		if (!visit[i] && (i == 0 || before != list[i])) {
+			visit[i] = true;
+			before = list[i];
+			arr[cnt] = list[i];
+			func(cnt + 1);
+			visit[i] = false;
+		}
+	}
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	init();
+
+	func(0);
+
+	return 0;
+}
