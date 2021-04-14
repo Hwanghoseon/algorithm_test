b331633520a4de3375e2988236b50b1559388216
Hwanghoseon <49743109+Hwanghoseon@users.noreply.github.com>
2021-04-14 17:08:11 +0900

    baekjoon - 가장 긴 감소하는 부분 수열

---
+++ 11722.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+#include<cmath>
+#include<algorithm>
+#include<string>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
+#include<cstdio>
+#include<climits>
+
+using namespace std;
+
+const int MAX = 1000 + 1;
+
+int N;
+int list[MAX];
+int dp[MAX];
+
+void input() {
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+		cin >> list[i];
+}
+
+int solution() {
+	for (int i = 1; i <= N; i++) {
+		dp[i] = 1;
+
+		for (int j = 1; j < i; j++)
+			if (list[i] < list[j])
+				dp[i] = max(dp[i], dp[j] + 1);
+	}
+
+	int ans = 0;
+
+	for (int i = 1; i <= N; i++)
+		ans = max(ans, dp[i]);
+
+	return ans;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	input();
+
+	cout << solution();
+
+	return 0;
+}
